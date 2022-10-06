325bc3022343f9478a2f18b3cdfd9557099bb5b7
jmbustos1 <jmbustos1@uc.cl>
2022-10-05 23:23:55 -0300

    entry test added

---
+++ db/sqlc/entry_test.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	_ "github.com/stretchr/testify/require"
+	"github.com/techschool/simplebank/util"
+)
+
+func createRandomEntry(t *testing.T, account Account) Entry {
+	arg := CreateEntriesParams{
+		AccountID: account.ID,
+		Amount:    util.RandomMoney(),
+	}
+
+	entry, err := testQueries.CreateEntries(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, arg.AccountID, entry.AccountID)
+	require.Equal(t, arg.Amount, entry.Amount)
+
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	account := createRandomAccount(t)
+	createRandomEntry(t, account)
+}
