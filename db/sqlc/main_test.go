2779d74c71cd13447cff4a00f9fbec89273ddcdd
jmbustos1 <jmbustos1@uc.cl>
2022-10-27 22:38:08 -0300

    POST GET LIST & viper env added

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,21 +6,32 @@ import (
 	"os"
 	"testing"
 
+	"github.com/jmbustos1/simplebank/util"
 	_ "github.com/lib/pq"
 )
 
+/*
 const (
 	dbDriver = "postgres"
 	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 	//dbSource = "postgresql://root:secret@localhost:5433/simple_bank2?sslmode=disable"
 )
+*/
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
+
 	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	var config util.Config
+
+	config, err = util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
--- main.go
+++ main.go
@@ -6,25 +6,33 @@ import (
 
 	"github.com/jmbustos1/simplebank/api"
 	db "github.com/jmbustos1/simplebank/db/sqlc"
+	"github.com/jmbustos1/simplebank/util"
 	_ "github.com/lib/pq"
 )
 
+/*
 const (
 	dbDriver = "postgres"
 	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 	//dbSource = "postgresql://root:secret@localhost:54
 	serverAdress = "0.0.0.0:8080"
 )
+*/
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot log config", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAdress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server")
 	}
